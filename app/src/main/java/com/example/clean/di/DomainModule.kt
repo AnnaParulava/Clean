2d04fe19ad62c289bd9b27fe047ca3bb53704f8e
AnnaParulava <ap554156@yandex.ru>
2023-07-16 18:25:25 +0300

    dagger DomainModule added

--- app/src/main/java/com/example/clean/di/DomainModule.kt
+++ app/src/main/java/com/example/clean/di/DomainModule.kt
@@ -1,4 +1,17 @@
 package com.example.clean.di
 
+import com.example.clean.domain.repository.UserRepository
+import com.example.clean.domain.usecase.GetUserNameUseCase
+import com.example.clean.domain.usecase.SaveUserNameUseCase
+import dagger.Module
+
+@Module
 class DomainModule {
+    fun provideGetUserNameUseCase(userRepository: UserRepository): GetUserNameUseCase{
+        return GetUserNameUseCase(userRepository)
+    }
+
+    fun provideSaveUserNameUseCase(userRepository: UserRepository): SaveUserNameUseCase{
+        return SaveUserNameUseCase(userRepository)
+    }
 }
