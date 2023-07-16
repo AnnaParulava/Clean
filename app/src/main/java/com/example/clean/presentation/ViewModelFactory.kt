2583893784e24efc7e133a5daa25ec2345c684f8
AnnaParulava <ap554156@yandex.ru>
2023-07-16 18:28:59 +0300

    ViewModelFactory updated

--- app/src/main/java/com/example/clean/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/clean/presentation/ViewModelFactory.kt
@@ -1,4 +1,19 @@
 package com.example.clean.presentation
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.clean.domain.usecase.GetUserNameUseCase
+import com.example.clean.domain.usecase.SaveUserNameUseCase
+
+class ViewModelFactory(
+    val getUserNameUseCase: GetUserNameUseCase,
+    val saveUserNameUseCase: SaveUserNameUseCase
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(
+            getUserNameUseCase,
+            saveUserNameUseCase
+        ) as T
+    }
 }
