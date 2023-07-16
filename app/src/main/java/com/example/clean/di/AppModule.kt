2bb89b139b20df1f730206c766cfe6d0b5061b02
AnnaParulava <ap554156@yandex.ru>
2023-07-16 18:31:09 +0300

    dagger AppModule added

--- app/src/main/java/com/example/clean/di/AppModule.kt
+++ app/src/main/java/com/example/clean/di/AppModule.kt
@@ -1,9 +1,20 @@
 package com.example.clean.di
 
 import androidx.transition.Visibility.Mode
+import com.example.clean.domain.usecase.GetUserNameUseCase
+import com.example.clean.domain.usecase.SaveUserNameUseCase
+import com.example.clean.presentation.ViewModelFactory
 import dagger.Module
 
 @Module
 class AppModule {
-
+    fun provideMainViewModelFactory(
+        getUserNameUseCase: GetUserNameUseCase,
+        saveUserNameUseCase: SaveUserNameUseCase
+    ): ViewModelFactory{
+        return ViewModelFactory(
+            getUserNameUseCase,
+            saveUserNameUseCase
+        )
+    }
 }
